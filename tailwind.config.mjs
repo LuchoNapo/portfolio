e0c7f0f2b4500c94aa6be4808e4bfd33202900cf
Lucho Napo <Luciano_napo@hotmail.com>
2024-10-08 12:33:56 -0300

    complete restylish of the portfolio

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,7 +6,24 @@ export default {
 			fontFamily: {
 				'nothing': ['"Nothing You Could Do"', 'cursive'],
 			},
+			keyframes: {
+				fadeUp: {
+					'0%': {
+						opacity: 0,
+						transform: 'translateY(50px)', // Inicia 50px más abajo
+					},
+					'100%': {
+						opacity: 1,
+						transform: 'translateY(0)', // Termina en la posición original
+					},
+				},
+			},
+			animation: {
+				fadeUp: 'fadeUp 1s ease-out forwards', // Animación de 1 segundo hacia arriba
+			},
+
 		},
 	},
+
 	plugins: [],
 }
