0e04c7095730a6902cfec25b641b74a698bb9fe7
Lucho Napo <Luciano_napo@hotmail.com>
2024-11-30 20:56:08 -0300

    [add] MateinaProject & [update] about image

--- src/i18n/translations/projects.ts
+++ src/i18n/translations/projects.ts
@@ -11,6 +11,7 @@ export const projects = {
         'project4.title': 'Wordpress Maintenance',
         'project4.body': 'Akapol website developed in WordPress, where I perform maintenance tasks to optimize its functionality and ensure its performance.',
 
+        'project5.body': 'An online store developed from scratch, allowing users to explore and purchase yerba mate products. It features a modern frontend and a fully functional backend for product management and integrated payments.',
     },
     es: {
         'project1.body': "Web desarrollada para Fabbri Desarrollos, donde se presenta el fideicomiso Ícaro 23, todas sus caracteristicas y especificaciones.",
@@ -22,6 +23,8 @@ export const projects = {
 
         'project4.title': 'Mantenimiento Wordpress',
         'project4.body': 'Aplicación desarrollada con la API de Rick and Morty, que permite explorar y visualizar información detallada sobre los personajes de la serie.',
+
+        'project5.body': 'Tienda en línea desarrollada desde cero, que permite explorar y comprar productos de yerba mate. Incluye un frontend moderno y un backend funcional para la gestión de productos y pasarela de pago.'
 
     },
 }
