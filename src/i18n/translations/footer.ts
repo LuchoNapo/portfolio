feaf6f54f74cd7a0e2f6266e3f616814019347dc
Lucho Napo <Luciano_napo@hotmail.com>
2025-03-11 15:05:42 -0300

    [update] curriculum en & es

--- src/i18n/translations/footer.ts
+++ src/i18n/translations/footer.ts
@@ -2,7 +2,6 @@ export const footer = {
     en: {
         "footer.curriculum": "Download my CV",
         "footer.contact": "Contact me!",
-
     },
     es: {
         "footer.curriculum": "Descargá mi CV",
